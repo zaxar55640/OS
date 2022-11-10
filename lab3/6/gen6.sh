b000f5be628ccaef03b9ce873e518629ba173dd3
zaxar55640 <90439895+zaxar55640@users.noreply.github.com>
2022-11-10 21:40:49 +0300

    Create gen6.sh

---
+++ lab3/6/gen6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+read line
+case "$line" in
+"TERM")
+kill -SIGTERM $(cat .pid)
+exit 0
+;;
+"+")
+kill -USR1 $(cat .pid)
+;;
+"*")
+kill -USR2 $(cat .pid)
+;;
+*)
+continue
+;;
+esac
+done
